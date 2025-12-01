e9dc8e24fa4a48277d15e3dcfb9fbebdf56d6e05
adriandk18 <adriandk757@gmail.com>
2025-12-01 14:13:49 +0700

    Uncomment Surat routes and add download route
    Uncommented routes for SuratController and added download route.

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,6 @@ use App\Http\Controllers\SuratController;
      return redirect('/surat');
  });
 
-// Route::resource('surat', SuratController::class);
-// Route::get('surat/{id}/pdf', [SuratController::class, 'pdf'])->name('surat.pdf');
-
 Route::get('/surat/verify', [SuratController::class, 'verifyPage'])->name('surat.verify');
 Route::post('/surat/verify/check', [SuratController::class, 'verifyProcess'])->name('surat.verify.process');
 
@@ -20,4 +17,3 @@ Route::get('/surat/{id}/preview', [SuratController::class, 'preview'])->name('su
 
 // Download PDF
 Route::get('/surat/{id}/download', [SuratController::class, 'download'])->name('surat.download');
-
